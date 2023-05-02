fa0b0cc5146edd8021bca57b5c277b2cbaf9ac6f
NLNet <yangqi1990917@163.com>
2023-05-02 19:55:36 +0800

    ● 示例支持变更检测，即时加载；

--- src/Nlnet.Avalonia.Css/Nlnet.Avalonia.Css.App/Views/MainWindow.axaml.cs
+++ src/Nlnet.Avalonia.Css/Nlnet.Avalonia.Css.App/Views/MainWindow.axaml.cs
@@ -1,36 +1,84 @@
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Styling;
+using Avalonia.Threading;
 
 namespace Nlnet.Avalonia.Css.App.Views
 {
     public partial class MainWindow : Window
     {
+        private static FileSystemWatcher? _watcher;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            Unloaded += MainWindow_Unloaded;
+        }
+
+        private static void MainWindow_Loaded(object? sender, RoutedEventArgs e)
+        {
+            _watcher                     =  new FileSystemWatcher("../../Assets");
+            _watcher.EnableRaisingEvents =  true;
+            _watcher.NotifyFilter        =  NotifyFilters.LastWrite;
+            _watcher.Filter              =  "*.css";
+            _watcher.Changed             += WatcherOnChanged;
+        }
+
+        private static void MainWindow_Unloaded(object? sender, RoutedEventArgs e)
+        {
+            _watcher?.Dispose();
+        }
+
+        private static void WatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+
+            ReloadCss();
         }
 
         private void ButtonLoadCss_OnClick(object? sender, RoutedEventArgs e)
         {
-            var parser  = new CssParser();
-            var cssFile = File.ReadAllText("../../Assets/avalonia.controls.css");
-            var cssStyles  = parser.TryGetStyles(cssFile);
+            ReloadCss();
+        }
 
-            foreach (var cssStyle in cssStyles)
+        private static void ReloadCss()
+        {
+            Task.Delay(50).ContinueWith(t =>
             {
-                //Trace.WriteLine(cssStyle.ToString());
-
-                var style = (cssStyle.ToAvaloniaStyle() as Style);
-                if (style?.Selector != null)
+                Dispatcher.UIThread.Post(() =>
                 {
-                    Application.Current?.Styles.Add(style);
-                }
-            }
+                    try
+                    {
+                        var parser    = new CssParser();
+                        var text      = File.ReadAllText("../../Assets/avalonia.controls.css");
+                        var cssStyles = parser.TryGetStyles(text);
+
+                        foreach (var cssStyle in cssStyles)
+                        {
+                            var style = (cssStyle.ToAvaloniaStyle() as Style);
+                            if (style?.Selector != null)
+                            {
+                                Application.Current?.Styles.Add(style);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                });
+            });
         }
     }
 }
