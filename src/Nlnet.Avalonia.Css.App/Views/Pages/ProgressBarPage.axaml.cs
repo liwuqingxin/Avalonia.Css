52874d6cdd02e84ee29c54f36df411cb34256997
NLNet <yangqi1990917@163.com>
2023-06-12 17:10:49 +0800

    ● 支持ProgressBar；

---
+++ src/Nlnet.Avalonia.Css.App/Views/Pages/ProgressBarPage.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Nlnet.Avalonia.Css.App.Views.Pages
+{
+    public partial class ProgressBarPage : UserControl
+    {
+        public ProgressBarPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Nlnet.Avalonia.Css.Fluent/Extensions/SliderExtension.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+
+namespace Nlnet.Avalonia.Css.Fluent;
+
+public class SliderExtension : AvaloniaObject
+{
+    public static string GetFormat(Visual host)
+    {
+        return host.GetValue(FormatProperty);
+    }
+    public static void SetFormat(Visual host, string value)
+    {
+        host.SetValue(FormatProperty, value);
+    }
+    public static readonly AttachedProperty<string> FormatProperty = AvaloniaProperty
+        .RegisterAttached<SliderExtension, Visual, string>("Format", "{0:F0}");
+}
