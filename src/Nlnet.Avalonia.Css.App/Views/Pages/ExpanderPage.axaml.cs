3c0ca45062bd9dbf3ae1424d77464ef76004f323
NLNet <yangqi1990917@163.com>
2023-06-23 21:13:49 +0800

    ● 支持Expander；

---
+++ src/Nlnet.Avalonia.Css.App/Views/Pages/ExpanderPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Nlnet.Avalonia.Css.App.Views.Pages
+{
+    public partial class ExpanderPage : UserControl
+    {
+        public ExpanderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
