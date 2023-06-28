86fb72c4b886cb16e4441d3b9e4923eede6f3129
NLNet <yangqi1990917@163.com>
2023-06-29 01:42:28 +0800

    ● 添加RhythmicTransformAnimator；（未完成）

---
+++ src/Nlnet.Avalonia.Css/Animators/RhythmicTransformAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Avalonia.Animation;
+using Avalonia.Animation.Animators;
+
+namespace Nlnet.Avalonia.Css
+{
+    /// <summary>
+    /// Animator that handles <see cref="T:Avalonia.Media.Transform" /> properties.
+    /// </summary>
+    public class RhythmicTransformAnimator : TransformAnimator
+    {
+        public RhythmicTransformAnimator()
+        {
+            
+        }
+
+        public override IDisposable BindAnimation(Animatable control, IObservable<double> instance)
+        {
+            int i = 0;
+            return base.BindAnimation(control, instance);
+        }
+    }
+}
