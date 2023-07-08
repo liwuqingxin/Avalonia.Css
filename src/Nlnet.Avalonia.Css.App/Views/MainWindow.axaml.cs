3d962e974d1585b4a615198a2a75ca433c5c2062
NLNet <yangqi1990917@163.com>
2023-07-08 14:01:33 +0800

    ● 整理Nlnet.Avalonia.SampleAssistant基本样式；

--- src/Nlnet.Avalonia.Css.App/Views/MainWindow.axaml.cs
+++ src/Nlnet.Avalonia.Css.App/Views/MainWindow.axaml.cs
@@ -18,43 +18,6 @@ namespace Nlnet.Avalonia.Css.App.Views
         {
             base.OnLoaded();
 
-            //var pageTypes = this.GetType().Assembly
-            //    .GetTypes()
-            //    .Where(t => t.Namespace != null && t.Namespace.StartsWith("Nlnet.Avalonia.Css.App.Views.Pages")
-            //        && t.IsAssignableTo(typeof(UserControl)));
-
-            //var assetLoader = AvaloniaLocator.Current.GetService<IAssetLoader>();
-            //TabControl.Items = pageTypes.Select(t =>
-            //{
-            //    var tabItem = new TabItem()
-            //    {
-            //        Content = Activator.CreateInstance(t),
-            //        Header  = t.Name[..^4],
-            //    };
-
-            //    if (assetLoader != null)
-            //    {
-            //        var imageUriString = $"avares://Nlnet.Avalonia.Css.App/Assets/Svg/{t.Name[..^4]}.svg";
-            //        if (assetLoader.Exists(new Uri(imageUriString)) == false)
-            //        {
-            //            imageUriString = $"avares://Nlnet.Avalonia.Css.App/Assets/Svg/default.svg";
-            //        }
-            //        var svg = new Icon()
-            //        {
-            //            Margin = new Thickness(12, 0, 0, 0),
-            //            Width  = 28,
-            //            Height = 28,
-            //        };
-            //        RenderOptions.SetBitmapInterpolationMode(svg, BitmapInterpolationMode.HighQuality);
-            //        LoadedStateMixin.SetUseLoadedState(svg, true);
-            //        Nlnet.Avalonia.Svg.Controls.Icon.SetIconSize(svg, 28);
-            //        Nlnet.Avalonia.Svg.Controls.Icon.SetIconSvg(svg, new Uri(imageUriString));
-            //        TabItemExtension.SetIconContent(tabItem, svg);
-            //    }
-
-            //    return tabItem;
-            //}).ToList();
-
             if (this.DataContext is MainWindowViewModel vm)
             {
                 vm.IsLoading = false;
