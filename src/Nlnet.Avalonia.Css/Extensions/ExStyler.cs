747aacb9b2e9bba628ce4eb3dc35ada8cb4820c1
NLNet <yangqi1990917@163.com>
2023-07-12 20:23:34 +0800

    ● 样式适应性修改；

--- src/Nlnet.Avalonia.Css/Extensions/ExStyler.cs
+++ src/Nlnet.Avalonia.Css/Extensions/ExStyler.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Disposables;
 using Avalonia;
-using Avalonia.Data;
-using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Styling;
-using DynamicData;
 
 namespace Nlnet.Avalonia.Css
 {
@@ -15,48 +11,6 @@ namespace Nlnet.Avalonia.Css
     /// </summary>
     internal class ExStyler : AvaloniaObject
     {
-        /// <summary>
-        /// Meaningless object. You should not get value from <see cref="IncludeProperty"/> normally.
-        /// </summary>
-        /// <param name="host"></param>
-        /// <returns></returns>
-        public static Uri GetInclude(StyledElement host)
-        {
-            return host.GetValue(IncludeProperty);
-        }
-        /// <summary>
-        /// Add a style resource to the <see cref="host"/>'s <see cref="StyledElement.Styles"/> if it is not existed.
-        /// </summary>
-        /// <param name="host"></param>
-        /// <param name="value"></param>
-        public static void SetInclude(StyledElement host, Uri value)
-        {
-            host.SetValue(IncludeProperty, value);
-        }
-        public static readonly AttachedProperty<Uri> IncludeProperty = AvaloniaProperty
-            .RegisterAttached<Styler, StyledElement, Uri>("Include");
-
-        /// <summary>
-        /// Get <see cref="Uri"/> of <see cref="StyleInclude"/> to remove.
-        /// </summary>
-        /// <param name="host"></param>
-        /// <returns></returns>
-        public static Uri? GetRemoveInclude(StyledElement host)
-        {
-            return host.GetValue(RemoveIncludeProperty);
-        }
-        /// <summary>
-        /// Set <see cref="Uri"/> of <see cref="StyleInclude"/> to remove.
-        /// </summary>
-        /// <param name="host"></param>
-        /// <param name="value"></param>
-        public static void SetRemoveInclude(StyledElement host, Uri? value)
-        {
-            host.SetValue(RemoveIncludeProperty, value);
-        }
-        public static readonly AttachedProperty<Uri?> RemoveIncludeProperty = AvaloniaProperty
-            .RegisterAttached<Styler, StyledElement, Uri?>("RemoveInclude");
-
         /// <summary>
         /// Get the list of <see cref="ICssStyle"/> instance that would be adding to the host.
         /// </summary>
@@ -82,34 +36,6 @@ namespace Nlnet.Avalonia.Css
 
         static ExStyler()
         {
-            IncludeProperty.Changed.AddClassHandler<StyledElement>((element, args) =>
-            {
-                if (args.NewValue is Uri uri && element.Styles.FirstOrDefault(s => s is StyleInclude include && include.Source == uri) == null)
-                {
-                    element.Styles.Add(new StyleInclude(uri)
-                    {
-                        Source = uri
-                    });
-                }
-            });
-
-            RemoveIncludeProperty.Changed.AddClassHandler<StyledElement>((element, args) =>
-            {
-                if (args.NewValue is not Uri uri)
-                {
-                    return;
-                }
-
-                var include = element.Styles.OfType<StyleInclude>().FirstOrDefault(include => include.Source == uri);
-                if (include != null)
-                {
-                    element.Styles.Remove(include);
-                }
-
-                // Maybe fail.
-                element.SetValue(RemoveIncludeProperty, null, BindingPriority.Style);
-            });
-
             AddingStyleProperty.Changed.AddClassHandler<StyledElement>((element, args) =>
             {
                 element.ClearValue(AddingStyleProperty);
