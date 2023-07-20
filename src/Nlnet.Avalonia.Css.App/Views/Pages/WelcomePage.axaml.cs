27330dda75528e68bf9b78f0229c45b28163da10
NLNet <yangqi1990917@163.com>
2023-07-20 21:12:19 +0800

    ● 添加Welcome页面；

---
+++ src/Nlnet.Avalonia.Css.App/Views/Pages/WelcomePage.axaml.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Nlnet.Avalonia.SampleAssistant;
+
+namespace Nlnet.Avalonia.Css.App.Views.Pages
+{
+    [GalleryItem("About Avalonia Css", Kind = GalleryItemKind.Welcome)]
+    public partial class WelcomePage : UserControl
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
