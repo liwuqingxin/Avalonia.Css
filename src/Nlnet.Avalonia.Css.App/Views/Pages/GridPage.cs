796827e428080f480535cd213c4b2826c706c448
NLNet <yangqi1990917@163.com>
2023-08-20 00:42:09 +0800

    + Theme: ContentControl; + Theme: DataValidationErrors; + Theme: EmbeddableControlRoot; + Theme: GridSplitter; + Theme: FlyoutPresenter; + Theme: ItemsControl; + Theme: Label;

---
+++ src/Nlnet.Avalonia.Css.App/Views/Pages/GridPage.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Nlnet.Avalonia.SampleAssistant;
+using System.ComponentModel;
+using Avalonia.Interactivity;
+using Nlnet.Avalonia.Controls;
+
+namespace Nlnet.Avalonia.Css.App.Views.Pages
+{
+    [GalleryItem("Grid")]
+    public partial class GridPage : UserControl
+    {
+        public GridPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
