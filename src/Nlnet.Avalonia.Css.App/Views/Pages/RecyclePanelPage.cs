147c34a5544cff50fb46e6d985000dbfff4e45dc
NLNet <yangqi1990917@163.com>
2023-08-20 16:34:14 +0800

    + RecyclePanel;

---
+++ src/Nlnet.Avalonia.Css.App/Views/Pages/RecyclePanelPage.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Nlnet.Avalonia.SampleAssistant;
+using System.ComponentModel;
+using Avalonia.Interactivity;
+using Nlnet.Avalonia.Controls;
+
+namespace Nlnet.Avalonia.Css.App.Views.Pages
+{
+    [GalleryItem("RecyclePanel")]
+    public partial class RecyclePanelPage : UserControl
+    {
+        public RecyclePanelPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
