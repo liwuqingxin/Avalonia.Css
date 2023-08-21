30fc49ca06409a82650ccdbc84a412741ffea210
NLNet <yangqi1990917@163.com>
2023-08-21 20:07:52 +0800

    + SplitView...

---
+++ src/Nlnet.Avalonia.Css.App/Views/Pages/SplitViewPage.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Nlnet.Avalonia.SampleAssistant;
+using System.ComponentModel;
+using Avalonia.Interactivity;
+using Nlnet.Avalonia.Controls;
+
+namespace Nlnet.Avalonia.Css.App.Views.Pages
+{
+    [GalleryItem("SplitView")]
+    public partial class SplitViewPage : UserControl
+    {
+        public SplitViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
