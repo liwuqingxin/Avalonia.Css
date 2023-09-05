927abfc0403d5fd8751fb2f88da2cee3b720adb3
NLNet <yangqi1990917@163.com>
2023-09-05 20:15:25 +0800

    + Icon Animation in ResourcePage;

--- src/Nlnet.Avalonia.Css.App/Views/Pages/ResourcesPage.axaml.cs
+++ src/Nlnet.Avalonia.Css.App/Views/Pages/ResourcesPage.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia.Controls;
 using Nlnet.Avalonia.SampleAssistant;
 using System.ComponentModel;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using System;
 
 namespace Nlnet.Avalonia.Css.App.Views.Pages
 {
@@ -10,6 +13,11 @@ namespace Nlnet.Avalonia.Css.App.Views.Pages
         public ResourcesPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_OnClick(object? sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.douyin.com/video/7269771109397056768") { UseShellExecute = true });
         }
     }
 }
